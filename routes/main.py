0d0cf8930427802b466d61eefe31fbdb32b241cc
mortdu1234 <dr05@orange.fr>
2026-05-22 13:56:06 +0200

    page de contact

--- routes/main.py
+++ routes/main.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, jsonify
 from routes import require_permission
+from backend.notifications import notifier
 
 main_bp = Blueprint("main", __name__)
 
@@ -17,6 +18,22 @@ def apropos():
 @main_bp.route("/contact")
 def contact():
     return render_template("contact.html")
+
+@main_bp.route("/contact/send", methods=["POST"])
+def sendMail():
+    data = request.get_json()
+    
+    nom     = data.get('nom', '').strip()
+    email   = data.get('email', '').strip()
+    message = data.get('message', '').strip()
+
+    success = notifier(f"Mail de:{nom}", f"from:{email}\nmessage:\n{message}", "high", ["Contact"])
+    if success:
+        return jsonify({'success': True, 'error': ''}), 200
+    return jsonify({'success': False, 'error': 'Champs manquants'}), 500
+    
+
+
 
 @main_bp.route("/tasks")
 def tasks():
